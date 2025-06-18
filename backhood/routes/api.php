a822aa317abf1a22e9fab8c73c6054c8a9e6ff5b
Katsis <katsis.k@outlook.com>
2025-06-18 22:51:05 +0300

    CMH-3 Remove hood by radius route

--- backhood/routes/api.php
+++ backhood/routes/api.php
@@ -7,5 +7,4 @@ Route::middleware('throttle:60,1')->group(function (): void {
     Route::get('/hoods/{uuid?}', [HoodRequestController::class, 'hoods']);
     Route::post('/hoods', [HoodRequestController::class, 'store']);
     Route::delete('/hoods/{uuid}', [HoodRequestController::class, 'delete']);
-    Route::delete('/hoods-by-radius', [HoodRequestController::class, 'hoodsByRadius']);
 });
