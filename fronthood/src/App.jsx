457b4e2f98d1d7772444838840b28f1437ce5047
Katsis <katsis.k@outlook.com>
2025-07-06 13:25:34 +0300

    CMH-5 Install leaflet to use a free map

--- fronthood/src/App.jsx
+++ fronthood/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import "leaflet/dist/leaflet.css";
 import hoodService from "./services/HoodService";
+import Map from "./components/Map";
 
 function App() {
     return (
         <>
+            <div>
+                <Map />
+            </div>
             <h1>Vite + React</h1>
             <div className="card">
                 <button onClick={() => hoodService.searchHoods()}>
