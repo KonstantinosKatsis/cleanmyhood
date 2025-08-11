fad62fadffd84ac42de5e828c79ff1851b7cf125
Katsis <katsis.k@outlook.com>
2025-08-12 01:56:08 +0300

    CMH-10 Update hood services for filtering

--- fronthood/src/services/RequestService.js
+++ fronthood/src/services/RequestService.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 
-export const getHoods = async (searchParameters) => {
-    const res = await axios.get("/api/hoods", {
+const getHoods = async (searchParameters) => {
+    // const url = uuid ? `/api/hoods/${uuid}` : `/api/hoods`;
+    const url = `/api/hoods`;
+    const res = await axios.get(url, {
         params: searchParameters,
     });
     return res.data;
 };
 
+const getHood = async (uuid) => {
+    const url = `/api/hoods/${uuid}`;
+    const res = await axios.get(url);
+    return res.data;
+};
+
 export default {
     getHoods,
+    getHood,
 };
